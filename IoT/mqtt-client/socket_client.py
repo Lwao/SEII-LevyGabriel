d9d6f62476d6a88d63c2072eb11e0df3d8464bea
leavitt <levygsg@hotmail.com>
2022-03-24 16:38:03 -0300

    Two way threads to rcv and send data in client and serer with reconnection

--- IoT/mqtt-client/socket_client.py
+++ IoT/mqtt-client/socket_client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 import time
 
 PORT = 50500 # port to connect to server
@@ -7,28 +8,40 @@ SERVER = "socket-server-app" # server ip address (ip address may change, so chec
 ADDR = (SERVER, PORT) # address tuple with client ip and port
 MSG_MAX_LEN = 1024 # message max. length
 
-def start_client_socket(conn):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # instantiate client socket with socket TCP
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # instantiate client socket with socket TCP
+client.connect(ADDR)
 
+def reconnect():
+    global client
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # instantiate client socket with socket TCP
+        client.connect(ADDR)
+    except: 
+        print('Reconnecting...')
+        time.sleep(.5)
+
+def handle_incoming_msg(conn):
     while True:
-        try:
-            if conn.poll(timeout=.1): 
-                mqtt_msg = conn.recv() # receive update message from mqtt client
-                if mqtt_msg: # exist mqtt_msg
-                    client.send(mqtt_msg.encode(FORMAT)) # forward mqtt update msg to server    
-            print('here')
+        try: 
             server_msg = client.recv(MSG_MAX_LEN).decode(FORMAT) # receive update message from socket server
             if server_msg: # exist server_msg
                 conn.send(server_msg) # forward server update msg to mqtt
-            print('pass')
-            time.sleep(.1)
-        except: # try to reconnect to server
-            try: 
-                print('Trying connection...')
-                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # instantiate client socket with socket TCP
-                client.connect(ADDR)
-                print('Connected!')
-            except socket.error as error: time.sleep(.1)
+        except: reconnect()
+
+def handle_forwarding_msg(conn):
+    while True:
+        if conn.poll(timeout=.1): 
+            mqtt_msg = conn.recv() # receive update message from mqtt client
+            if mqtt_msg: # exist mqtt_msg
+                try: client.send(mqtt_msg.encode(FORMAT)) # forward mqtt update msg to server  
+                except: reconnect()
+
+def start_client_socket(conn):
+    thread_rcv = threading.Thread(target=handle_incoming_msg, args=(conn,)) # thread to receive messages from server
+    thread_send = threading.Thread(target=handle_forwarding_msg, args=(conn,)) # thread to send messages to server
+
+    thread_rcv.start()
+    thread_send.start()
 
 if __name__ == "__main__":
     start_client_socket()
