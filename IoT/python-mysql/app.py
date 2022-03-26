bdbb36c006686915a206c278f09c8f68a7cb9d87
leavitt <levygsg@hotmail.com>
2022-03-26 00:48:00 -0300

    Minor db update (build fail)

--- IoT/python-mysql/app.py
+++ IoT/python-mysql/app.py
@@ -1,4 +1,21 @@
 # python -m pipreqs.pipreqs
 
-if __name__ == "__main__":
-    print('Hello world!')
+import mysql.connector as mysql
+
+db = mysql.connect(
+    host = "db",
+    user = "root",
+    passwd = "",
+    database = "iotdashboard"
+)
+
+cursor = db.cursor()
+
+## getting all the tables which are present in 'iotdatabase' database
+cursor.execute("SHOW TABLES")
+
+tables = cursor.fetchall() ## it returns list of tables present in the database
+
+## showing all the tables one by one
+for table in tables:
+    print(table)
